dbc21c59e3676d625037656dd033c5b10da77f88
Naung-Kendrick <naungkendrick@gmail.com>
2026-05-01 15:33:38 +0630

    Install Tailwind as PostCSS plugin, remove CDN, add tailwind.config.cjs and postcss.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mm: ['"Noto Serif Myanmar"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
